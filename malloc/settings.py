156543bd4dc9e22f3cf989fbde5c1a6bbfb63564
keerthivasan123 <keerthivasan287@gmail.com>
2019-02-26 22:04:52 +0530

    added media root

--- malloc/settings.py
+++ malloc/settings.py
@@ -125,3 +125,17 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'malloc/static')
 ]
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
