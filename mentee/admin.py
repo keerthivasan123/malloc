1899c9936a439e8196af6678f0653ab304d6d6fc
keerthivasan123 <keerthivasan287@gmail.com>
2019-02-28 15:14:48 +0530

    admin for mentee

--- mentee/admin.py
+++ mentee/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import startup_Details
+from .models import team_member
 
 # Register your models here.
+admin.site.register(startup_Details)
+admin.site.register(team_member)
