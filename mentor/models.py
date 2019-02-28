3d03a7b278696de0d83905360f1b44bdbe0ad72a
keerthivasan123 <keerthivasan287@gmail.com>
2019-02-28 11:13:35 +0530

    models for mentors

--- mentor/models.py
+++ mentor/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
 # Create your models here.
+class Mentor(models.Model):
+    mentorname = models.CharField(max_length=264)
+    mentorphoto = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    mentorgmailid = models.CharField(max_length=264)
+    mentorphoneno = models.IntegerField()
+    state = models.CharField(max_length=264)
+    city = models.CharField(max_length=264)
+    mentorspecialisation = models.CharField(max_length=264)
+
+
+def __str__(self):
+    return self.mentorname, self.mentorgmailid
