3c9e41f497cd733c8e508331effc9e595b6c26b6
keerthivasan123 <keerthivasan287@gmail.com>
2019-03-01 22:59:26 +0530

    added filters and other properties in mentee admin

--- mentee/admin.py
+++ mentee/admin.py
@@ -3,5 +3,17 @@ from .models import startup_Details
 from .models import team_member
 
 # Register your models here.
-admin.site.register(startup_Details)
-admin.site.register(team_member)
+class MenteeAdmin(admin.ModelAdmin):
+  list_display = ( 'startupname', 'state', 'city', 'natureofthecompany','mentor')
+  list_display_links = ('startupname','mentor',)
+  list_filter = ('startupname','state','city','mentor')
+  search_fields = ('startupname', 'state', 'city', 'natureofthecompany', 'Udhyogaadhar')
+  list_per_page = 6
+admin.site.register(startup_Details,MenteeAdmin)
+class team_member_Admin(admin.ModelAdmin):
+  list_display = ( 'startup','membername', 'memberrole','linkedinurl')
+  list_display_links = ('startup',)
+  list_filter = ('startup',)
+  search_fields = ('startupname', 'memberrole')
+  list_per_page = 6
+admin.site.register(team_member,team_member_Admin)
