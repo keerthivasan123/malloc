fba165f3d326f8e6bbdb0ff36f3fc64c7afaf835
keerthivasan123 <keerthivasan287@gmail.com>
2019-03-08 18:42:27 +0530

    Updated urls.py

--- mentee/urls.py
+++ mentee/urls.py
@@ -3,8 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard', views.index, name='index'),
-    path('', views.register, name='index')
+    path('dashboard', views.index, name='dashboard'),
+    path('', views.register, name='index'),
+    path('dashboard/connect/mentor/', views.my_view, name='map'),
+    path('dashboard/connect', views.project, name='project'),
+    path('dashboard/connect/rate', views.rate, name='rate'),
+    path('dashboard/rating', views.rating1, name='rating'),
+    
 ]
 
     
